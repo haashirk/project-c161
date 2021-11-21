f7de6cbb491eaba6904e75466d40b539d056f414
haashirk <73969551+haashirk@users.noreply.github.com>
2021-11-21 11:42:14 -0600

    Add files via upload

---
+++ c161Project-master/js/throw.js
@@ -0,0 +1,28 @@
+AFRAME.registerComponent("bowling-balls", {
+    init: function() {
+      this.throwBall();
+    },
+    throwBall: function() {
+      window.addEventListener("keydown", 
+      (event) => {
+        if (event.key == "a") {
+          var ball = document.createElement("a-entity");
+          ball.setAttribute("gltf-model", "./models/bowling-ball/scene.gltf");
+          ball.setAttribute("scale", { x: 3, y: 3,  z: 3});
+          var cam = document.querySelector("#camera");
+          pos = cam.getAttribute("position");
+          ball.setAttribute("position", {
+            x: pos.x,
+            y: pos.y-1.2,
+            z: pos.z,
+          });
+          var camera = document.querySelector("#camera").object3D;
+          var direction = new THREE.Vector3();
+          camera.getWorldDirection(direction);
+          ball.setAttribute("velocity", direction.multiplyScalar(-10)); 
+          var scene = document.querySelector("#scene");
+          scene.appendChild(ball);
+        }
+      });
+    },
+  });
